5ae5535703efdcde62c22ec793682d070c97d9a7
Alexie Staffer <alexie.staffer@gmail.com>
2016-09-26 22:26:00 +0100

    Initial Commit

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string name)
+	{
+		Debug.Log (string.Format ("Level load requested: {0}", name));
+	}
+
+	public void UserQuit ()
+	{
+		Debug.Log ("Quit requested.");
+	}
+}
